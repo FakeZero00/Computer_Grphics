5dabfe36dfc4d77e220ff8e1d19b819bcfaca739
FakeZero <sjlimdaa@naver.com>
2026-09-01 23:27:14 +0900

    데이터 읽기 성공

--- 2. Warming_Up2/main.cpp
+++ 2. Warming_Up2/main.cpp
@@ -1,11 +1,46 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<ranges>
+#include<fstream>
+#include<sstream>
 using namespace std;
 
 int main()
 {
-	cout << "Hello, World!" << endl;
+	ifstream in { "data.txt" };
+	if (not in) {
+		cout << "파일을 열 수 없습니다." << endl;
+		system("pause");
+		return 20260901;
+	}
+
+	vector<vector<string>> data;
+	string line;
+
+	while (getline(in, line)) {
+		stringstream ss(line);
+		string word;
+		vector<string> row;
+
+		while (ss >> word) {
+			row.push_back(word);
+		}
+
+		if (!row.empty()) {
+			data.push_back(row);
+		}
+	}
+
+	cout << "읽어온 데이터:" << endl;
+	for(const auto& row : data) {
+		for(const auto& word : row) {
+			cout << word << " ";
+		}
+		cout << endl;
+	}
+
+
 
 	system("pause");
 }
