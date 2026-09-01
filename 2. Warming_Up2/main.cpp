441081b9cc5c0d46fa9d61097b76b113d29dff44
FakeZero <sjlimdaa@naver.com>
2026-09-02 03:01:45 +0900

    워밍업2 진행중

--- 2. Warming_Up2/main.cpp
+++ 2. Warming_Up2/main.cpp
@@ -4,7 +4,42 @@
 #include<ranges>
 #include<fstream>
 #include<sstream>
+#include<cctype>
+#include<windows.h>
 using namespace std;
+
+bool cboolean = false;
+
+void printData(const vector<vector<string>>& data) {
+	if (!cboolean)
+	{
+		for (const auto& row : data) {
+			for (const auto& word : row) {
+				cout << word << " ";
+			}
+			cout << endl;
+		}
+	}
+	else {
+		for (auto& row : data) {
+			int count = 0;
+			for (auto& word : row) {
+				if (isupper(word[0])) {
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+					cout << word << " ";
+					count++;
+				}
+				else {
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+					cout << word << " ";
+				}
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+
+			}
+			cout << "[대문자로 시작되는 단어 개수]: " << count << endl;
+		}
+	}
+}
 
 int main()
 {
@@ -16,6 +51,7 @@ int main()
 	}
 
 	vector<vector<string>> data;
+	vector<vector<string>> originData;
 	string line;
 
 	while (getline(in, line)) {
@@ -31,16 +67,58 @@ int main()
 			data.push_back(row);
 		}
 	}
+	originData = data;
+
+	bool isRunning = true;
+	string command;
 
 	cout << "읽어온 데이터:" << endl;
-	for(const auto& row : data) {
-		for(const auto& word : row) {
-			cout << word << " ";
+	printData(data);
+
+	while (isRunning){
+		cout << "명령어를 입력하세요: ";
+		cin >> command;
+
+		//대소문자 역전
+		if (command == "a") {
+			for (auto& row : data) {
+				for (auto& word : row) {
+					for (char& c : word) {
+						if (isupper(c)) {
+							c = tolower(c);
+						}
+						else if (islower(c)) {
+							c = toupper(c);
+						}
+					}
+				}
+			}
+			printData(data);
+			cout << endl;
 		}
-		cout << endl;
+
+		//문장에 쓰인 단어 개수 출력
+		else if (command == "b") {
+			for (auto& row : data) {
+				for (auto& word : row) {
+					cout << word << " ";
+				}
+				cout << "[단어 개수]: " << row.size() << endl;
+			}
+			cout << endl;
+		}
+
+		//대문자로 시작되는 단어를 찾아서 그 단어를 찾아 그 단어를 다른 색으로 출력, 개수도 출력
+		else if (command == "c") {
+			cboolean = !cboolean;
+			printData(data);
+			cout << endl;
+		}
+
+		//프로그램 종료
+		else if (command == "q") isRunning = false;
+		else cout << "잘못된 명령어입니다." << endl;
 	}
-
-
 
 	system("pause");
 }
