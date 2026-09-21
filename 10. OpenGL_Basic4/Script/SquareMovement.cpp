972235302b47fb70ec109eb30db1b43deef7a3f8
FakeZero(Laptop) <sjlimdaa@naver.com>
2026-09-21 10:36:52 +0900

    과제 4 하는 중

--- 10. OpenGL_Basic4/Script/SquareMovement.cpp
+++ 10. OpenGL_Basic4/Script/SquareMovement.cpp
@@ -13,6 +13,8 @@ void SquareMovement::Update(float deltaTime) {
 		if (MoveMode == 1) MoveMode = 0;
 		else MoveMode = 1;
 
+		if (clockmove == true) clockmove = false;
+
 		random_device rd;
 		default_random_engine dre{ rd() };
 		uniform_real_distribution<float> urd{ -1.0f, 1.0f };
@@ -24,6 +26,8 @@ void SquareMovement::Update(float deltaTime) {
 		if (MoveMode == 2) MoveMode = 0;
 		else MoveMode = 2;
 
+		if (clockmove == true) clockmove = false;
+
 		random_device rd;
 		default_random_engine dre{ rd() };
 		uniform_real_distribution<float> urd{ -1.0f, 1.0f };
@@ -31,11 +35,22 @@ void SquareMovement::Update(float deltaTime) {
 		velocity.x = urd(dre);
 		velocity.y = 0.0f;
 	}
+	else if (inputManager.GetKeyDown(GLFW_KEY_3)) {
+		if (MoveMode == 3) MoveMode = 0;
+		else MoveMode = 3;
+
+		random_device rd;
+		default_random_engine dre{ rd() };
+		uniform_real_distribution<float> urd{ -1.0f, 1.0f };
+
+		velocity.x = urd(dre);
+		velocity.y = urd(dre);
+	}
 
 	transform->position.x += velocity.x * deltaTime;
 	transform->position.y += velocity.y * deltaTime;
 
-	if (MoveMode <= 3 && clockmove == false ) {
+	if (MoveMode == 1 || MoveMode == 3 && clockmove == false ) {
 		if (transform->position.x - (renderer->width / 2.0f) < -1.0f ||
 			transform->position.x + (renderer->width / 2.0f) > 1.0f) {
 			if (MoveMode == 3) clockmove = true;
@@ -47,8 +62,27 @@ void SquareMovement::Update(float deltaTime) {
 			else velocity.y = -velocity.y;
 		}
 	}
+	else if (MoveMode == 2 && zigzag == false) {
+		if (timer > 0.0f) {
+			timer -= deltaTime;
+			return;
+		}
+		else {
+			velocity.x = 1.0f;
+			velocity.y = 0.0f;
+			timer = 0.0f;
+		}
+		if (transform->position.x - (renderer->width / 2.0f) < -1.0f ||
+			transform->position.x + (renderer->width / 2.0f) > 1.0f) {
+			velocity.x = -velocity.x;
+		}
+		else if (transform->position.x - (renderer->width / 2.0f) < -border) {
+			velocity.x = 0.0f;
+			velocity.y = 1.0f;
+			timer = 0.1f;
+		}
+	}
 	else if (MoveMode == 3 && clockmove == true) {
-		float border = 0.95f;
 		if (transform->position.x + (renderer->width / 2.0f) > border &&
 			transform->position.y + (renderer->height / 2.0f) > border) {
 			velocity.x = -1.0f;
--- 10. OpenGL_Basic4/Script/SquareMovement.h
+++ 10. OpenGL_Basic4/Script/SquareMovement.h
@@ -10,7 +10,10 @@ public:
 private:
 	//제어변수
 	int MoveMode = 0;
+	bool zigzag = false;
 	bool clockmove = false;
+	float border = 1.0f;
+	float timer = 0.0f;
 
 	//속도
 	Position2D velocity{ 0.0f, 0.0f };
